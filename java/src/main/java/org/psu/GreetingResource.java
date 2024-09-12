cebad881a742ee927e95e7c9eaf8c11a15c7fefa
bjmyers <bmyers0217@gmail.com>
2024-09-12 19:08:50 -0400

    Add Javadoc

--- java/src/main/java/org/psu/GreetingResource.java
+++ java/src/main/java/org/psu/GreetingResource.java
@@ -5,6 +5,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+/**
+ * Basic resource to return a hello world response
+ */
 @Path("/hello")
 public class GreetingResource {
 
