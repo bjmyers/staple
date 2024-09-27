543bbd52174359b673f077a6ee4459986c5c54de
bjmyers <bmyers0217@gmail.com>
2024-09-27 12:27:47 -0400

    Change compilation target to Windows

--- java/buildExecutable.sh
+++ java/buildExecutable.sh
@@ -1,2 +1,2 @@
 # Disable jar because of gradle tooling, see https://github.com/quarkusio/quarkus/discussions/40679
-./gradlew build -Dquarkus.native.enabled=true -Dquarkus.package.jar.enabled=false
+./gradlew build -Dquarkus.native.enabled=true -Dquarkus.package.jar.enabled=false -Dquarkus.native.target=windows
