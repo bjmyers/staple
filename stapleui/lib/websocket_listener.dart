76afc39db08231673ac04e1fc11c5258bc60badb
bjmyers <bmyers0217@gmail.com>
2024-11-04 19:12:16 -0500

    Display Ships

--- stapleui/lib/websocket_listener.dart
+++ stapleui/lib/websocket_listener.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stapleui/credit/credit_state.dart';
 import 'package:stapleui/credit/credit_message.dart';
+import 'package:stapleui/ship/ship_message.dart';
+import 'package:stapleui/ship/ship_state.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WebsocketListener {
@@ -16,11 +18,16 @@ class WebsocketListener {
       final Map<String, dynamic> json = jsonDecode(message);
       final String type = json["type"];
       final CreditState creditState = Provider.of<CreditState>(context, listen: false);
+      final ShipState shipState = Provider.of<ShipState>(context, listen: false);
 
       switch (type) {
         case "Credit":
           final creditMessage = CreditMessage.fromJson(json);
           creditState.updateCredits(creditMessage.totalCredits);
+          break;
+        case "Ships":
+          final shipMessage = ShipMessage.fromJson(json);
+          shipState.updateShipData(shipMessage.ships);
           break;
         default:
           break;
