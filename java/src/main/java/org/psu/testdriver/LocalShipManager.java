20af4d4db472ec2273ce717c585b8decb9e12e3e
bjmyers <bmyers0217@gmail.com>
2024-11-17 17:14:50 -0500

    Test Driver Returns Ships

---
+++ java/src/main/java/org/psu/testdriver/LocalShipManager.java
@@ -0,0 +1,56 @@
+package org.psu.testdriver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.psu.spacetraders.dto.Ship;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.extern.jbosslog.JBossLog;
+
+@JBossLog
+@ApplicationScoped
+public class LocalShipManager {
+
+	private final ObjectMapper objectMapper;
+	private List<Ship> ships;
+
+	@Inject
+	public LocalShipManager() {
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		this.objectMapper = mapper;
+
+		this.ships = null;
+	}
+
+	public List<Ship> getShips() {
+		if (this.ships == null) {
+			lazyLoadShips();
+		}
+		return this.ships;
+	}
+
+	private void lazyLoadShips() {
+
+		try (final InputStream is = this.getClass().getResourceAsStream("/testDriverData/ships.json")) {
+			final JavaType shipList = objectMapper.getTypeFactory().constructCollectionLikeType(List.class, Ship.class);
+			try {
+				this.ships = objectMapper.readValue(is, shipList);
+			} catch (Exception e) {
+				log.error(e);
+			}
+		} catch (IOException e) {
+			log.error(e);
+		}
+
+		log.infof("Local Ship Manager loaded %s ships", this.ships.size());
+	}
+
+}
--- java/src/main/java/org/psu/testdriver/client/LocalShipsClient.java
+++ java/src/main/java/org/psu/testdriver/client/LocalShipsClient.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.psu.spacetraders.api.ShipsClient;
 import org.psu.spacetraders.dto.DataWrapper;
+import org.psu.spacetraders.dto.DataWrapper.WrapperMetadata;
 import org.psu.spacetraders.dto.Ship;
+import org.psu.testdriver.LocalShipManager;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 /**
  * Local version of the {@link ShipsClient}
@@ -14,9 +17,17 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class LocalShipsClient implements ShipsClient {
 
+	@Inject
+	private LocalShipManager shipManager;
+
 	@Override
 	public DataWrapper<List<Ship>> getShips(int limit, int page) {
-		return null;
+		final List<Ship> ships = shipManager.getShips();
+		final WrapperMetadata metadata = new WrapperMetadata(ships.size(), page, limit);
+		if (page == 1) {
+			return new DataWrapper<>(ships, metadata);
+		}
+		return new DataWrapper<>(List.of(), metadata);
 	}
 
 }
